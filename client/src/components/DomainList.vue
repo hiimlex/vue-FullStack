39de099baad8594c4abbaf94fb78a37cc927dac5
Alex <alex.adaumi@gmail.com>
2019-11-11 09:16:27 -0300

    terminando aula 7 e entendendo promises e async await

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -1,165 +1,153 @@
 <template>
-  <div>
-    <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
-            <AppItemList
-              :items="prefixes"
-              @addItem="addPrefix"
-              @deleteItem="deletePrefix"
-              title="Prefixos"
-            ></AppItemList>
-          </div>
-          <div class="col-md">
-            <AppItemList
-              :items="sufixes"
-              @addItem="addSufix"
-              @deleteItem="deleteSufix"
-              title="Sufixos"
-            ></AppItemList>
-          </div>
-        </div>
-        <br />
-        <h5>
-          Domains
-          <span class="badge badge-info">{{domains.length}}</span>
-        </h5>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" :key="domain">
-                <div class="row">
-                  <div class="col-md">{{domain}}</div>
-                  <div class="col-md text-right">
-                    <button class="btn btn-info">
-                      <span class="fa fa-shopping-cart"></span>
-                    </button>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md">
+				<AppItemList title="Prefixos" type="prefix" v-bind:items="items.prefix" v-on:addItem="addItem" v-on:deleteItem="deleteItem"></AppItemList>
+			</div>
+			<div class="col-md">
+				<AppItemList title="Sufixos" type="suffix" v-bind:items="items.suffix" v-on:addItem="addItem" v-on:deleteItem="deleteItem"></AppItemList>
+			</div>
+		</div>
+		<br/>
+		<h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+		<div class="card">
+			<div class="card-body">
+				<ul class="list-group">
+					<li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+						<div class="row">
+							<div class="col-md-6">
+								{{ domain.name }}
+							</div>
+							<div class="col-md-3">
+								<span class="badge badge-info">{{ (domain.available) ? "Disponível" : "Não Disponível" }}</span>
+							</div>
+							<div class="col-md-3 text-right">
+								<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+									<span class="fa fa-shopping-cart"></span>
+								</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import axios from "axios/dist/axios";
 import AppItemList from "./AppItemList";
-
 export default {
-  name: "app",
-  components: { AppItemList },
-  data() {
-    return {
-      prefixes: [],
-      sufixes: []
-    };
-  },
-  methods: {
-    addPrefix(prefix) {
-      axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-                mutation ($item: ItemInput) {
-                    newPrefix: saveItem(item: $item){
-                        id
-                        type
-                        description
-                    }
-                }
-            `,
-          variables: {
-            item: {
-              type: "prefix",
-              description: prefix
-            }
-          }
-        }
-      }).then(response => {
-        const query = responde.data;
-        const newPrefix = query.data.newPrefix;
-        this.prefixes.push(newPrefix.description);
-      });
-    },
-    addSufix(sufix) {
-      this.sufixes.push(sufix);
-    },
-    deletePrefix(prefix) {
-	axios({
-		url: "http://localhost:4000",
-		method: "post",
-		data: {
-			query: `
-				mutation{
-					deleteItem
+	name: "app",
+	components: {
+		AppItemList
+	},
+	data: function () {
+		return {
+			items: {
+				prefix: [],
+				suffix: []
+			},
+			domains: []
+		};
+	},
+	methods: {
+		addItem(item) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation ($item: ItemInput) {
+							newItem: saveItem(item: $item) {
+								id
+								type
+								description
+							}
+						}
+					`,
+					variables: {
+						item
+					}
 				}
-			`
-		}
-		}).then();
-    },
-    deleteSufix(sufix) {
-      this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
-    },
-    getPrefixes() {
-      axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-					{
-						prefixes: items (type: "prefix") {
-						id
+			}).then(response => {
+				const query = response.data;
+				const newItem = query.data.newItem;
+				this.items[item.type].push(newItem);
+				this.generateDomains();
+			});
+		},
+		deleteItem(item) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation ($id: Int) {
+							deleted: deleteItem(id: $id)
+						}
+					`,
+					variables: {
+						id: item.id
+					}
+				}
+			}).then(() => {
+				this.items[item.type].splice(this.items[item.type].indexOf(item), 1);
+				this.generateDomains();
+			});
+		},
+		getItems(type) {
+			return axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						query ($type: String) {
+							items: items (type: $type) {
+								id
+								type
+								description
+							}
+						}
+					`,
+					variables: {
 						type
-						description
-						}
 					}
-				`
-        }
-      }).then(response => {
-        const query = response.data;
-        this.prefixes = query.data.prefixes;
-      });
-    },
-    getSuffixes() {
-      axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-					{
-						sufixes: items (type: "sufix") {
-							description
+				}
+			}).then(response => {
+				const query = response.data;
+				this.items[type] = query.data.items;
+			});
+		},
+		generateDomains() {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation {
+							domains: generateDomains {
+								name
+								checkout
+								available
+							}
 						}
-					}
-				`
-        }
-      }).then(response => {
-        const query = response.data;
-        this.sufixes = query.data.sufixes;
-      });
-    }
-  },
-  computed: {
-    domains() {
-      const domains = [];
-      for (const prefix of this.prefixes) {
-        for (const sufix of this.sufixes) {
-          domains.push(prefix.description + sufix.description);
-        }
-      }
-      return domains;
-    }
-  },
-  created() {
-    this.getPrefixes();
-    this.getSuffixes();
-  }
+					`
+				}
+			}).then((response) => {
+				const query = response.data;
+				this.domains = query.data.domains;
+			});
+		}
+	},
+	created() {
+		Promise.all([
+			this.getItems("prefix"),
+			this.getItems("suffix")
+		]).then(() => {
+			this.generateDomains();
+		});
+	}
 };
 </script>
 
--- server/server.js
+++ server/server.js
@@ -1,38 +1,52 @@
 const { ApolloServer } = require("apollo-server");
-//declarando schemas
+const dns = require("dns");
+
 const typeDefs = `
-
-    type Item{
-        id: Int
-        type: String
-        description: String
-    }
-
-    type Query{
-        items ( type: String ): [Item]  
-    }
-
-    input ItemInput{
-      type: String
-      description: String
-    }
-
-    type Mutation{
-        saveItem(item: ItemInput): Item
-        deleteItem(id: Int): Boolean
-    }
+	type Item {
+		id: Int
+		type: String
+		description: String
+	}
+	type Domain {
+		name: String
+		checkout: String
+		available: Boolean
+	}
+	type Query {
+		items(type: String): [Item]
+	}
+	input ItemInput {
+		type: String
+		description: String
+	}
+	type Mutation {
+		saveItem(item: ItemInput): Item
+		deleteItem(id: Int): Boolean
+		generateDomains: [Domain]
+	}
 `;
 
 const items = [
       { id: 1, type: "prefix", description: "Air" },
       { id: 2, type: "prefix", description: "Jet" },
       { id: 3, type: "prefix", description: "Flight" },
-      { id: 4, type: "sufix", description: "Hub" },
-      { id: 5, type: "sufix", description: "Station" },
-      { id: 6, type: "sufix", description: "Mart" }
+      { id: 4, type: "suffix", description: "Hub" },
+      { id: 5, type: "suffix", description: "Station" },
+      { id: 6, type: "suffix", description: "Mart" },
 ];
 
-//resolvers são as responsaveis por tratarem o objeto
+const isDomainAvailable = function (url) {
+      return new Promise(function (resolve, reject) {
+            dns.resolve(url, function (error) {
+                  if (error) {
+                        resolve(true);
+                  } else {
+                        resolve(false);
+                  }
+            });
+      });
+};
+
 const resolvers = {
       Query: {
             items(_, args) {
@@ -42,7 +56,6 @@ const resolvers = {
       Mutation: {
             saveItem(_, args) {
                   const item = args.item;
-                  //GErando número aleatório
                   item.id = Math.floor(Math.random() * 1000);
                   items.push(item);
                   return item;
@@ -53,6 +66,23 @@ const resolvers = {
                   if (!item) return false;
                   items.splice(items.indexOf(item), 1);
                   return true;
+            },
+            async generateDomains() {
+                  const domains = [];
+                  for (const prefix of items.filter(item => item.type === "prefix")) {
+                        for (const suffix of items.filter(item => item.type === "suffix")) {
+                              const name = prefix.description + suffix.description;
+                              const url = name.toLowerCase();
+                              const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+                              const available = await isDomainAvailable(`${url}.com.br`);
+                              domains.push({
+                                    name,
+                                    checkout,
+                                    available
+                              });
+                        }
+                  }
+                  return domains;
             }
       }
 };
